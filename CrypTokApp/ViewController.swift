1328a4a3acb7bd92085d4dbdd387510aa59f21ec
Sideeq <sideeqw@gmail.com>
2021-12-15 14:08:43 -0500

    Initial Commit

---
+++ CrypTokApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrypTokApp
+//
+//  Created by Sideeq on 12/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
